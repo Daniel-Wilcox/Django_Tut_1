dec586453429401c473e58109b2c291aa3d485fe
Daniel <d.dt.wilcox@gmail.com>
2021-03-17 15:49:36 +0200

    completed tut 8

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -12,9 +12,9 @@ export default class App extends Component{
     }
     render() {
         return (
-        <div className="center">
-            <HomePage />
-        </div>
+            <div className="center">
+                <HomePage />
+            </div>
         );
     }
 }
